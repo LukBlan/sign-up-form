e0737d14d9ed815f2ac3f79bc3ae21063d5ab279
Lukblan <luk.blanko@gmail.com>
2023-01-17 20:43:27 -0300

    Password is now checked in case they are not equal

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
 const inputs = document.querySelectorAll("input");
+const passwordInput = document.querySelectorAll("input[type='password']");
 inputs.forEach(input => input.addEventListener("focusout", styleInput))
+passwordInput.forEach(input => input.addEventListener("focusout", checkPassword))
+
+
 
 function styleInput(event) {
-  event.target.className = (event.target.checkValidity())? "valid-input": "invalid-input";
+  event.target.className = (event.target.checkValidity())? "": "invalid-input";
+}
+
+function checkPassword() {
+  const inputPassword = document.getElementById("password");
+  const inputRepeatPassword = document.getElementById("repeat-password");
+  const userPassword = inputPassword.value;
+  const userRepeatPassword = inputRepeatPassword.value;
+
+  if (userPassword !== "" && userRepeatPassword !== "" && userPassword !== userRepeatPassword) {
+    inputPassword.className = "invalid-input";
+    inputRepeatPassword.className = "invalid-input";
+  } else if (userPassword === userRepeatPassword && userPassword !== ""){
+    inputPassword.className = "valid-input";
+    inputRepeatPassword.className = "valid-input";
+  }
 }
