0712729300b2928a59a7183ffdecc876522ef473
Lukblan <luk.blanko@gmail.com>
2023-02-13 14:38:41 -0300

    Change variable name on function

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@
   document.body.addEventListener("focusout", checkInputValidity);
 
   function checkInputValidity(event) {
-    const inputElement = event.target;
-    if (inputElement.type.includes("password")) {
-      passwordValidity()
-    } else {
-      textValidity(inputElement);
+    const element = event.target;
+    if (element.nodeName === "INPUT") {
+      if (element.type.includes("password")) {
+        passwordValidity()
+      } else {
+        textValidity(element);
+      }
     }
   }
 
@@ -25,7 +27,7 @@
       if (!element.className.includes("invalid-input")) {
         element.classList.add("invalid-input")
       }
-      if (!element.className.includes("valid-input")) {
+      if (element.className.includes("valid-input")) {
         element.classList.remove("valid-input")
       }
     }
@@ -35,15 +37,16 @@
     const userPassword = inputPassword.value;
     const userRepeatPassword = inputRepeatPassword.value;
 
-    if (userPassword === userRepeatPassword && userPassword !== "" && userRepeatPassword !== ""){
+    if (userPassword === userRepeatPassword && userPassword !== "" && userRepeatPassword !== "") {
       inputPassword.className = "valid-input";
       inputRepeatPassword.className = "valid-input";
       inputPassword.setCustomValidity('');
       inputRepeatPassword.setCustomValidity('');
     } else {
       inputPassword.className = "invalid-input";
-      inputPassword.setCustomValidity("Invalid field.");
       inputRepeatPassword.className = "invalid-input";
+      inputPassword.setCustomValidity("Invalid field.");
+
       inputRepeatPassword.setCustomValidity("Invalid field.");
     }
   }
