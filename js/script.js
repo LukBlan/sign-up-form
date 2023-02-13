c63be9ec62311a1a0ed6d678949909351dde66b3
Lukblan <luk.blanko@gmail.com>
2023-02-13 14:01:02 -0300

    Modular refactor: add inputs module, change eventListeners

--- js/script.js
+++ js/script.js
@@ -1,32 +1,55 @@
-const inputs = document.querySelectorAll("input");
-const passwordInput = document.querySelectorAll("input[type='password']");
-inputs.forEach(input => input.addEventListener("focusout", styleInput))
-passwordInput.forEach(input => input.addEventListener("focusout", checkPassword))
-
-function styleInput(event) {
-  if(event.target.checkValidity()) {
-    event.target.className = ""
-  } else {
-    event.target.className ="invalid-input";
-  }
-}
-
-function checkPassword() {
+(function inputs() {
   const inputPassword = document.getElementById("password");
   const inputRepeatPassword = document.getElementById("repeat-password");
-  const userPassword = inputPassword.value;
-  const userRepeatPassword = inputRepeatPassword.value;
 
+  document.body.addEventListener("focusout", checkInputValidity);
 
- if (userPassword === userRepeatPassword && userPassword !== "" && userRepeatPassword !== ""){
-    inputPassword.className = "valid-input";
-    inputRepeatPassword.className = "valid-input";
-    inputPassword.setCustomValidity('');
-    inputRepeatPassword.setCustomValidity('');
-  } else {
-    inputPassword.className = "invalid-input";
-    inputPassword.setCustomValidity("Invalid field.");
-    inputRepeatPassword.className = "invalid-input";
-    inputRepeatPassword.setCustomValidity("Invalid field.");
+  function checkInputValidity(event) {
+    const inputElement = event.target;
+    if (inputElement.type.includes("password")) {
+      passwordValidity()
+    } else {
+      textValidity(inputElement);
+    }
   }
-}
+
+  function textValidity(element) {
+    if(element.checkValidity()) {
+      if (element.className.includes("invalid-input")) {
+        element.classList.remove("invalid-input")
+      }
+      if (!element.className.includes("valid-input")) {
+        element.classList.add("valid-input")
+      }
+    } else {
+      if (!element.className.includes("invalid-input")) {
+        element.classList.add("invalid-input")
+      }
+      if (!element.className.includes("valid-input")) {
+        element.classList.remove("valid-input")
+      }
+    }
+  }
+
+  function passwordValidity() {
+    const userPassword = inputPassword.value;
+    const userRepeatPassword = inputRepeatPassword.value;
+
+    if (userPassword === userRepeatPassword && userPassword !== "" && userRepeatPassword !== ""){
+      inputPassword.className = "valid-input";
+      inputRepeatPassword.className = "valid-input";
+      inputPassword.setCustomValidity('');
+      inputRepeatPassword.setCustomValidity('');
+    } else {
+      inputPassword.className = "invalid-input";
+      inputPassword.setCustomValidity("Invalid field.");
+      inputRepeatPassword.className = "invalid-input";
+      inputRepeatPassword.setCustomValidity("Invalid field.");
+    }
+  }
+})()
+
+
+
+
+
